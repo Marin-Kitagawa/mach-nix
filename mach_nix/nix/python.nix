cc40ad7fcb5f91375b42c04edc9ebf5b81e7c1de
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2026-02-23 09:20:04 +0530

    chore(Python): Changed Python version from 3.9 to 3.14

--- mach_nix/nix/python.nix
+++ mach_nix/nix/python.nix
@@ -1,20 +1,24 @@
 {
-  pkgs ? import (import ./nixpkgs-src.nix) { config = {}; overlays = []; },
+  pkgs ? import (import ./nixpkgs-src.nix) {
+    config = { };
+    overlays = [ ];
+  },
   dev ? false,
-  extraModules ? [],
+  extraModules ? [ ],
   ...
 }:
 let
   lib = pkgs.lib;
-  python = pkgs.python39;
-  pythonDeps = (lib.attrValues (import ./python-deps.nix { inherit python; fetchurl = pkgs.fetchurl; }));
+  python = pkgs.python314;
+  pythonDeps = lib.attrValues (
+    import ./python-deps.nix {
+      inherit python;
+      fetchurl = pkgs.fetchurl;
+    }
+  );
   pythonDepsDev = with python.pkgs; [
     pytest_6
     pytest-xdist
   ];
 in
-python.withPackages ( ps:
-  pythonDeps
-  ++ (lib.optionals dev pythonDepsDev)
-  ++ extraModules
-)
+python.withPackages (ps: pythonDeps ++ (lib.optionals dev pythonDepsDev) ++ extraModules)
